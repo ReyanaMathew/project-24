11fa836364f5a0c748450ce7dc0beba3db6e0724
ReyanaMathew <74352665+ReyanaMathew@users.noreply.github.com>
2021-08-31 15:41:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+    rubber=new Rubber(900,450,70)
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    stone=new Stone(700,320,100,100)
+   i=new iron(300,350)
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+    rubber.display();
+    plane.display();
+    hammer.display();
+    i.display();
+    stone.display();
+ 
+}
